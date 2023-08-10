8258cd5be0a552960724edd4e86af3842ff6431d
Basit Qayoom Chowdhary <basitqayoomchowdhary@Basit-12905.local>
2023-08-10 14:59:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import "stencil-pw/pw/main"
+function App() {
+  return (
+    <div className="App">
+      <h1>Stencil in react</h1>
+        <>
+        <my-form></my-form>
+        <my-timer></my-timer>
+        <my-input></my-input>
+        <my-button></my-button>
+        <text-field text="Hi from stencil & react"></text-field>
+        </>
+    </div>
+  );
+}
+
+export default App;
